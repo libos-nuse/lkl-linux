c22927329ce4486a7732f93de84169de80368dbf
Hajime Tazaki <thehajime@gmail.com>
2018-01-19 21:40:55 +0900

    lkl: fix FreeBSD compilation errors
    Various header includsion are now ifdef-ed with __FreeBSD__ or __linux__
    switches. hijack library is now skipped to build (may need more ifdefs
    for lib/hijack/ codes).
    
    It also avoid to use sysctl to check nproc.
    
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- tools/lkl/lib/virtio_net_macvtap.c
+++ tools/lkl/lib/virtio_net_macvtap.c
@@ -15,17 +15,26 @@
  * sudo chown thehajime /dev/tap22
  */
 
+#include <stdio.h>
 #include <net/if.h>
+#ifdef __linux__
 #include <linux/if_tun.h>
+#endif
 
 #include "virtio.h"
 #include "virtio_net_fd.h"
 
 struct lkl_netdev *lkl_netdev_macvtap_create(const char *path, int offload)
 {
+#ifdef __linux__
 	struct ifreq ifr = {
 		.ifr_flags = IFF_TAP | IFF_NO_PI,
 	};
 
 	return lkl_netdev_tap_init(path, offload, &ifr);
+#else
+	fprintf(stderr, "%s: macvtap isn't available on this platform\n",
+		path);
+	return NULL;
+#endif
 }
